dbe7f66a6e7efb239a8abde8f34ca43feb28cece
Dezmond-sama <Dezmond-sama@mail.ru>
2024-03-13 12:38:27 +0300

    Todo formetted with different prettier. SQL Formatter

--- todo.sql
+++ todo.sql
@@ -1,18 +1,16 @@
-CREATE TABLE
-    users (
-        user_id serial PRIMARY KEY,
-        firstname varchar(24) NOT NULL,
-        lastname varchar(64) NOT NULL,
-        username varchar(64) NOT NULL
-    );
+CREATE TABLE users (
+    user_id serial PRIMARY KEY,
+    firstname varchar(24) NOT NULL,
+    lastname varchar(64) NOT NULL,
+    username varchar(64) NOT NULL
+);
 
-CREATE TABLE
-    tasks (
-        task_id serial PRIMARY KEY,
-        title varchar(256),
-        create_date timestamp NOT NULL DEFAULT now (),
-        due_date timestamp,
-        finish_date timestamp,
-        user_id integer NOT NULL,
-        FOREIGN KEY (user_id) REFERENCES users (user_id)
-    );
+CREATE TABLE tasks (
+    task_id serial PRIMARY KEY,
+    title varchar(256),
+    create_date timestamp NOT NULL DEFAULT now (),
+    due_date timestamp,
+    finish_date timestamp,
+    user_id integer NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
